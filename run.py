3f9ab961f270f895580315227f4f083ab8dae1a3
root <root@DESKTOP-A8B4IU1.localdomain>
2019-07-14 18:10:44 -0400

    Adds CLI interface for searching repos
    - Uses click to allow user input for organization name
    - Allows option parameter sort_by

--- run.py
+++ run.py
@@ -1,35 +1,43 @@
 import requests
 import requests_cache
 import json
+import click
 
 requests_cache.install_cache(cache_name='github_cache', backend='sqlite', expire_after=180)
-org_request = requests.get('https://api.github.com/orgs/github')
 
-if not org_request.ok:
-    raise Exception
+@click.command()
+@click.option("--org", prompt="Enter organization name",
+              help="Name of organization to search repositories for (Required)")
+@click.option("--sort_by",
+              help="Options: stargazers_count, forks_count, contributors_count")
+def organization_repos(org, sort_by):
+    org_request = requests.get(f'https://api.github.com/orgs/{org}')
 
-org_info = json.loads(org_request.content or org_request.text)
-repos_request = requests.get(org_info.get('repos_url'))
+    if not org_request.ok:
+        org_request.raise_for_status()
 
-if not repos_request:
-    raise Exception
+    org_info = json.loads(org_request.content or org_request.text)
+    repos_request = requests.get(org_info.get('repos_url'))
 
-repos_info = json.loads(repos_request.content or repos_request.text)
+    if not repos_request:
+        repos_request.raise_for_status()
 
-for i, repo in enumerate(repos_info):
-    contributors_request = requests.get(repo.get('contributors_url'))
-            
-    if not contributors_request:
-        raise Exception
+    repos_info = json.loads(repos_request.content or repos_request.text)
+    
+    if sort_by and sort_by in ['stargazers_count', 'forks_count', 'contributors_count']:
+        if sort_by == "contributors_count":
+            for i, repo in enumerate(repos_info):
+                contributors_request = requests.get(repo.get('contributors_url'))
+                        
+                if not contributors_request:
+                    contributors_request.raise_for_status()
 
-    contributors_info =  json.loads(contributors_request.content or contributors_request.text)
-    repos_info[i]['contributors_count'] = len(contributors_info)
-
-
-for s in ['stargazers_count', 'forks_count', 'contributors_count']:
-    print(s)
-    a = sorted(repos_info, key=lambda x: x.get(s))
+                contributors_info =  json.loads(contributors_request.content or contributors_request.text)
+                repos_info[i]['contributors_count'] = len(contributors_info)
         
-    for repo in a:
-        print(repo.get(s))
+        sorted(repos_info, key=lambda x: x.get(sort_by))
+    print(repos_info)
+    return repos_info
 
+if __name__ == '__main__':
+    organization_repos()
